6d41077194e7bffda40dd7cb3aeb7d8c6be53837
justin <prostodevs@gmail.com>
2022-09-14 11:03:20 -0400

    pokelist images and cards added

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -8,51 +8,52 @@ const pokeListNav = document.querySelector("#pokeList");
 const homeNav = document.querySelector("#home");
 
 document.querySelector("#home").addEventListener("click", (e) => {
-    e.preventDefault()
-    mainBody.innerHTML = createHome();
-    homeNav.classList.add("active")
-    teamListNav.classList.remove("active")
-    pokeListNav.classList.remove("active")
-    localStorage.setItem('activePage', 'home')
+  e.preventDefault();
+  mainBody.innerHTML = createHome();
+  homeNav.classList.add("active");
+  teamListNav.classList.remove("active");
+  pokeListNav.classList.remove("active");
+  localStorage.setItem("activePage", "home");
 });
 
 document.querySelector("#pokeList").addEventListener("click", (e) => {
-    e.preventDefault()
-    mainBody.innerHTML = createPokeList();
-    pokeListNav.classList.add("active")
-    teamListNav.classList.remove("active")
-    homeNav.classList.remove("active")
-    localStorage.setItem('activePage', 'pokeList')
-})
+  e.preventDefault();
+  //createPokeList({ parentElt: mainBody });
+  mainBody.innerHTML = createPokeList();
+  pokeListNav.classList.add("active");
+  teamListNav.classList.remove("active");
+  homeNav.classList.remove("active");
+  localStorage.setItem("activePage", "pokeList");
+});
 
 document.querySelector("#teamList").addEventListener("click", (e) => {
-    e.preventDefault()
-    mainBody.innerHTML = createTeam();
-    teamListNav.classList.add("active")
-    homeNav.classList.remove("active")
-    pokeListNav.classList.remove("active")
-    localStorage.setItem('activePage', 'teamList')
-})
+  e.preventDefault();
+  mainBody.innerHTML = createTeam();
+  teamListNav.classList.add("active");
+  homeNav.classList.remove("active");
+  pokeListNav.classList.remove("active");
+  localStorage.setItem("activePage", "teamList");
+});
 
 window.onload = () => {
-    const activePage = localStorage.getItem('activePage')
-    
-    switch (activePage) {
-        case "home":
-            mainBody.innerHTML = createHome()
-            homeNav.classList.add("active")
-            break;
-        case "pokeList":
-            mainBody.innerHTML = createPokeList()
-            pokeListNav.classList.add("active")
-            break;
-        case "teamList":
-            mainBody.innerHTML = createTeam()
-            teamListNav.classList.add("active")
-            break;
-        default:
-            mainBody.innerHTML = createHome()
-            homeNav.classList.add("active")
-            break;
-    }
-}
+  const activePage = localStorage.getItem("activePage");
+
+  switch (activePage) {
+    case "home":
+      mainBody.innerHTML = createHome();
+      homeNav.classList.add("active");
+      break;
+    case "pokeList":
+      mainBody.innerHTML = createPokeList();
+      pokeListNav.classList.add("active");
+      break;
+    case "teamList":
+      mainBody.innerHTML = createTeam();
+      teamListNav.classList.add("active");
+      break;
+    default:
+      mainBody.innerHTML = createHome();
+      homeNav.classList.add("active");
+      break;
+  }
+};
